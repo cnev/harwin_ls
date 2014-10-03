e2ba09a0010a0531124a45903c39f90bfd747b0f
Chandara NEV <cnev@e3r6p5.42.fr>
2014-10-03 17:55:04 +0200

    c

--- nev_argchecks.c
+++ nev_argchecks.c
@@ -1,33 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   nev_ls.h                                           :+:      :+:    :+:   */
+/*   nev_argchecks.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cnev <cnev@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2014/10/03 17:45:54 by cnev              #+#    #+#             */
-/*   Updated: 2014/10/03 17:45:55 by cnev             ###   ########.fr       */
+/*   Created: 2014/10/03 17:48:57 by cnev              #+#    #+#             */
+/*   Updated: 2014/10/03 17:48:57 by cnev             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LS_H
-# define LS_H
-# include "stdlib.h"
+#include "../includes/ls.h"
 
-typedef struct		s_queue
+int				read_command(int ac, char **av, t_ls *info)
 {
-	char			*dir;
-	struct s_queue	*next;
-}					t_queue;
-
-typedef struct		s_ls
-{
-	int				flag_l;
-	int				flag_r;
-	int				flag_a;
-	int				flag_R;
-	int				flag_t;
-	t_queue			*dirlst;
-}					t_ls;
-
-#endif
+	if (ac == 1)
+		zero_flags(info);
+	else if (ac == 2)
+	{
+		if ((detect_flags(info)) == -1)
+			return (-1);
+	}
+	info->dirlst = NULL;
+	return (0);
+}
